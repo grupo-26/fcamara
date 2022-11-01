3794b66d46edaa2d3a5008a11307a77249bf9c54
Elton <rs.eltonribeiro@gmail.com>
2022-11-01 17:05:10 -0300

    feat: adicionado rota e view de homepage e configurações para servir arquivo estático

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.send("Olá, FCamara!");
+  // res.send("Olá, FCamara!");
+  res.render("home");
 })
 
 app.listen(8000, () => {

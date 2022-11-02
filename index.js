7eafc05056404122998acc3353eac5ef7b7c926f
Elton <rs.eltonribeiro@gmail.com>
2022-11-02 17:53:50 -0300

    feat: adicionado algumas rotas básicas e templates em EJS

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const userRouter = require("./routes/users");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", userRouter);
 
 app.get("/", (req, res) => {
   // res.send("Olá, FCamara!");
